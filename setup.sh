b90c529ec4739bb8dc2e736d636919229155b807
9rotama <aoim99.sc@gmail.com>
2025-01-01 18:44:18 +0900

    zeliij

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 cd "$(dirname "$0")" || exit 
 SCRIPT_DIR=$(pwd)
 
-ln -fs $SCRIPT_DIR/config/.zshrc $HOME/.config/.zshrc
+ln -fs $SCRIPT_DIR/config/.zshrc $HOME/.zshrc
+ln -fs $SCRIPT_DIR/config/zellij/config.kdl $HOME/.config/zellij/config.kdl
 ln -fs $SCRIPT_DIR/config/starship.toml $HOME/.config/starship.toml
 ln -fs $SCRIPT_DIR/config/alacritty/alacritty.toml $HOME/.config/alacritty/alacritty.toml
 ln -fs $SCRIPT_DIR/config/alacritty/catppuccin-mocha.toml $HOME/.config/alacritty/catppuccin-mocha.toml
